529f2533eaa11bc8917ea60829e6dd3aadf5fa9b
Joris van der Wel <joris@jorisvanderwel.com>
2014-12-01 17:19:31 +0100

    From now on, the project is released as AGPL 3!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,25 @@
+/*
+ * DealPort
+ * Copyright (c) 2014  DealPort B.V.
+ *
+ * This file is part of DealPort
+ *
+ * DealPort is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * DealPort is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with DealPort.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, the following supplemental terms apply, based on section 7 of
+ * the GNU Affero General Public License (version 3):
+ * a) Preservation of all legal notices and author attributions
+ */
 'use strict';
 
 module.exports = function(grunt)
@@ -84,5 +106,5 @@ module.exports = function(grunt)
         grunt.registerTask('lint', ['jshint']);
         grunt.registerTask('no-css', ['jshint', 'primuslib', 'browserify', 'compress:bundlejs']);
         grunt.registerTask('no-lint', ['primuslib', 'browserify', 'compress:bundlejs', 'stylerefs', 'less', 'compress:bundlecss']);
-        grunt.registerTask('default', ['jshint', 'primuslib', 'browserify', 'compress:bundlejs', 'stylerefs', 'less', 'compress:bundlecss']);
+        grunt.registerTask('default', ['jshint', 'primuslib', 'browserify', 'compress:bundlejs', /*'stylerefs',*/ 'less', 'compress:bundlecss']);
 };
